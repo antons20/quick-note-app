3523b4fd7e528e7bdecca869935e14b5c127ef40
Anton Shtarkman <ashtark3@jhu.edu>
2021-10-22 21:53:32 -0400

    Added to Note.js

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,12 +1,55 @@
-import { ListItem, ListItemText } from "@material-ui/core"
+import React, { Component } from "react";
 
-function Note(props) {
-    const { note } = props;
-    return (
-        <ListItem>
-            <ListItemText primary = {note.title} secondary = {note.text}/>
-        </ListItem>
-    );
+import { List, ListItem, ListItemText, Collapse, ListItemIcon, Button} from "@material-ui/core";
+import { ExpandMore, ExpandLess , Delete} from "@material-ui/icons"
+
+
+class Note extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+
+    handleClick = () => {
+        this.setState({ open: !this.state.open})
+    }
+
+    render() {
+        const { note, deleteNote } = this.props;
+        const { open } = this.state;
+        return (
+            <>
+                <ListItem >
+                    <ListItemIcon onClick={this.handleClick}>
+                        {open ? <ExpandLess /> : <ExpandMore />}
+                    </ListItemIcon>
+                    <ListItemText primary = {note.title}/>
+
+
+                    <ListItemIcon>
+                        <Button onClick={() => deleteNote(note)}> 
+                            <Delete />
+                        </Button>
+                    </ListItemIcon>
+
+                    
+                </ListItem>
+
+                <Collapse in={open} timeout="auto" unmountOnExit> 
+                    <List component= "div" disablePadding>
+                        <ListItem>
+                            <ListItemText secondary = {note.text}/>
+                        </ListItem>
+                    </List>
+                </Collapse>
+                
+            </>
+        );
+    }
+
+
 }
 
 export default Note;
